8e3df74cbab35604618b2b1e4eea73b000c8c164
DroidStudioAS <as.droiddevteam@gmail.com>
2024-04-15 18:21:53 +0200

    imported jquery for frontend toggling, and added comment display form

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--JQuery-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <link rel="stylesheet" href="{{asset("/css/main.css")}}">
     <title>@yield("title", "Catalogue")</title>
 </head>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,15 +2,34 @@
 @section("content")
     @foreach($products as $product)
         <div class="product_card">
-            <img src="{{\App\Helpers\ProductHelper::buildImagePath($product)}}" alt="product_image" class="product_image">
-            <div class="product_data">
-                <div class="product_name_desc">
-                    <p>{{$product->brand->name}}  {{$product->name}}</p>
-                    <p>{{$product->description}}</p>
+            <div class="product_content">
+                <img src="{{\App\Helpers\ProductHelper::buildImagePath($product)}}" alt="product_image" class="product_image">
+                <div class="product_data">
+                    <div class="product_name_desc">
+                        <p>{{$product->brand->name}}  {{$product->name}}</p>
+                        <p>{{$product->description}}</p>
+                    </div>
+                    <p>{{$product->price}}$</p>
                 </div>
-                <p>{{$product->price}}$</p>
             </div>
+            <div class="action_buttons">
+                <div onclick="displayComments({{$product->id}})" class="comments_toggle">
+                    Show Comments
+                </div>
+                <div id="{{$product->id . "commentContainer"}}" class="comments">
+                    @foreach($product->comments as $comment)
+                        <p>{{$comment->name}} {{$comment->email}}</p>
+                        <p>{{$comment->comment}}</p>
+                        <p>{{$comment->created_at}}</p>
+                    @endforeach
+                </div>
+            </div>
+
         </div>
     @endforeach
-
+<script>
+    function displayComments(id){
+        $("#"+id+"commentContainer").css("display","flex");
+    }
+</script>
 @endsection
