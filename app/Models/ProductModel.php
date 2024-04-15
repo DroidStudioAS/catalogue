9117bbc9ca3dc79bec9e1077f42f16fd542964a9
DroidStudioAS <as.droiddevteam@gmail.com>
2024-04-15 16:08:49 +0200

    added brand and product models

---
+++ app/Models/ProductModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductModel extends Model
+{
+    protected $table = "products";
+
+    protected $fillable = ["brand_id", "name", "image_path","description"];
+
+    public function brand(){
+        return $this->hasOne(BrandModel::class, "id","brand_id");
+    }
+}
