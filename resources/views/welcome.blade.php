e8b26cdfc45fd5d8ad121525c937c20556f608b1
DroidStudioAS <as.droiddevteam@gmail.com>
2024-04-15 17:51:27 +0200

    added stylesheet relationship to layout file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,6 @@
 @extends("layouts.layout")
 @section("content")
-    @foreach($products as $product)
-        <img src="{{\App\Helpers\ProductHelper::buildImagePath($product)}}"/>
-        <p>
-            {{$product->brand->name}}
-        </p>
-        <p>
-            {{$product->name}}
-        </p>
-        <p>
-            {{$product->description}}
-        </p>
-        <p>
-            {{$product->price}}
-        </p>
-    @endforeach
+    <div class="product_card">
+
+    </div>
 @endsection
