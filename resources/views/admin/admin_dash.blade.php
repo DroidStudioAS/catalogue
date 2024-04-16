22fb0ef9b40c8d961ca8ca2ba6843076eb316d22
DroidStudioAS <as.droiddevteam@gmail.com>
2024-04-16 16:35:58 +0200

    increased comment border rounding

--- resources/views/admin/admin_dash.blade.php
+++ resources/views/admin/admin_dash.blade.php
@@ -1,23 +1,27 @@
 @extends("layouts.layout")
 @section("content")
-    @foreach($comments as $comment)
-       <div class="comment_card">
-           <p>Name: {{$comment->name}}</p>
-           <p>Email: {{$comment->email}}</p>
-           <p>For: {{$comment->product->brand->name}}  {{$comment->product->name}}</p>
-           <p>Comment: {{$comment->comment}}</p>
-           <p>Status: {{$comment->status}}</p>
+    <h3>Approve Comments</h3>
+    <div class="product_container">
+        @foreach($comments as $key=>$comment)
+            <div class="{{$key%2===0? "comment_card_even" : "comment_card_uneven"}}">
+                <p>Name: {{$comment->name}}</p>
+                <p>Email: {{$comment->email}}</p>
+                <p>For: {{$comment->product->brand->name}}  {{$comment->product->name}}</p>
+                <p>Comment: {{$comment->comment}}</p>
+                <p>Status: {{$comment->status}}</p>
 
-           <form action="{{route("admin.comment",["comment"=>$comment])}}" method="POST">
-               {{csrf_field()}}
-                <button>
-                    @if($comment->status==="pending")
-                        Approve Comment
-                    @else
-                         Ban Comment
-                    @endif
-                </button>
-           </form>
-       </div>
-    @endforeach
+                <form action="{{route("admin.comment",["comment"=>$comment])}}" method="POST">
+                    {{csrf_field()}}
+                    <button>
+                        @if($comment->status==="pending")
+                            Approve Comment
+                        @else
+                            Ban Comment
+                        @endif
+                    </button>
+                </form>
+            </div>
+        @endforeach
+    </div>
+
 @endsection
