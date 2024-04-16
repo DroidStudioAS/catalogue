d765347d93c3a50ba4b8a7d63e485128adb85078
DroidStudioAS <as.droiddevteam@gmail.com>
2024-04-16 07:33:10 +0200

    rendering comments on adminpanel

--- resources/views/admin/admin_dash.blade.php
+++ resources/views/admin/admin_dash.blade.php
@@ -1,6 +1,19 @@
 @extends("layouts.layout")
 @section("content")
     @foreach($comments as $comment)
-        <p>{{$comment}}</p>
+       <div class="comment_card">
+           <p>Name: {{$comment->name}}</p>
+           <p>Email: {{$comment->email}}</p>
+           <p>For: {{$comment->product->brand->name}}  {{$comment->product->name}}</p>
+           <p>Comment: {{$comment->comment}}</p>
+           <p>Status: {{$comment->status}}</p>
+           <button>
+               @if($comment->status==="pending")
+                   Approve Comment
+               @else
+                    Ban Comment
+               @endif
+           </button>
+       </div>
     @endforeach
 @endsection
