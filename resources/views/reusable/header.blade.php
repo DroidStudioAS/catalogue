81049ba0711bd74ecd9f93250b0a7419bb418efb
DroidStudioAS <as.droiddevteam@gmail.com>
2024-04-16 08:27:26 +0200

    added toggling of navigation on the frontend (blank navigation)

--- resources/views/reusable/header.blade.php
+++ resources/views/reusable/header.blade.php
@@ -1,3 +1,20 @@
-<header>
+<header class="header">
     <h1 class="app_title">SladIT Car Saloon</h1>
+    <img onclick="toggleMenu()" class="menu_button" src="/res/icon_nav.png">
+    <nav class="navigation">
+
+    </nav>
 </header>
+<script>
+    let isExpanded = false;
+    function toggleMenu(){
+        let right = "0px";
+        if (isExpanded){
+            right="-300px";
+        }
+        $(".navigation").animate({
+            right:right
+        },500)
+        isExpanded=!isExpanded;
+    }
+</script>
