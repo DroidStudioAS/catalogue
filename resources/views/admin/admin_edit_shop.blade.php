0a5b4ee58d647e7dac04bebdb863cc42b2d4443d
DroidStudioAS <as.droiddevteam@gmail.com>
2024-04-16 17:59:42 +0200

    edit form pre filled with product data

--- resources/views/admin/admin_edit_shop.blade.php
+++ resources/views/admin/admin_edit_shop.blade.php
@@ -1,8 +1,10 @@
 @extends("layouts.layout")
 @section("content")
-    <form class="form" action="{{route("admin.shop.edit",["product"=>$product])}}" method="POST">
+    <h2>Editing {{$product->brand->name}} {{$product->name}}</h2>
+    <form class="edit_form" action="{{route("admin.shop.edit",["product"=>$product])}}" method="POST">
         {{csrf_field()}}
-        <select>
+        <label for="brand_id">Brand</label>
+        <select name="brand_id" class="input_text">
             @foreach($categories as $category)
                 <option @if($category->id===$product->brand->id) selected @endif
                     value="{{$category->id}}">
@@ -10,9 +12,14 @@
                 </option>
             @endforeach
         </select>
-        <input type="text" name="name" id="">
-        <textarea type="text" name="description" id=""></textarea>
-        <input type="number">
+        <label for="name">Model</label>
+        <input class="input_text" type="text" name="name" value="{{$product->name}}">
+        <label for="brand_id">Description</label>
+        <textarea class="input_textarea" type="text" name="description" id="">{{$product->description}}</textarea>
+        <label for="price">Price ($)</label>
+        <input class="input_text" type="number" name="price" value="{{$product->price}}">
+        <label for="image_name">Image</label>
+        <input class="input_text" type="file" name="image_name">
         <input type="submit">
 
 
