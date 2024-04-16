6e06418949666c3d1fd0002cef470942808b8009
DroidStudioAS <as.droiddevteam@gmail.com>
2024-04-16 10:19:22 +0200

    fixed css error where the product cards would realign after comment expansion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,47 +1,50 @@
 @extends("layouts.layout")
 @section("content")
-    @foreach($products as $product)
-        <div class="product_card">
-            <div class="product_content">
-                <img src="{{\App\Helpers\ProductHelper::buildImagePath($product)}}" alt="product_image" class="product_image">
-                <div class="product_data">
-                    <div class="product_name_desc">
-                        <p>{{$product->brand->name}}, {{$product->name}}</p>
-                        <p>{{$product->description}}</p>
-                    </div>
-                    <p>{{$product->price}}$</p>
-                </div>
-            </div>
-            <div class="action_buttons">
-                <div id="{{"displayComments" . $product->id}}" onclick="displayComments({{$product->id}})" class="comments_toggle">
-                    Show Comments
-                </div>
-                <div id="{{$product->id . "commentContainer"}}" class="comments">
-                    @foreach($product->approvedComments as $comment)
-                    <div class="comment">
-                        <div class="comment_signature">
-                            <p>{{$comment->name}} {{$comment->email}}</p>
-                            <p>{{\Carbon\Carbon::parse($comment->created_at)->format("Y-m-d")}}</p>
+    <div class="product_container">
+        @foreach($products as $product)
+            <div class="product_card">
+                <div class="product_content">
+                    <img src="{{\App\Helpers\ProductHelper::buildImagePath($product)}}" alt="product_image" class="product_image">
+                    <div class="product_data">
+                        <div class="product_name_desc">
+                            <p>{{$product->brand->name}}, {{$product->name}}</p>
+                            <p>{{$product->description}}</p>
                         </div>
-                        <p>{{$comment->comment}}</p>
+                        <p>{{$product->price}}$</p>
                     </div>
-                    @endforeach
-                    <form method="POST" action="{{route("add.comment")}}" class="comment">
-                        {{csrf_field()}}
-                        <input type="hidden" name="product_id" value="{{$product->id}}">
-                        <div class="comment_signature">
-                            <input type="text" name="name" placeholder="name">
-                            <input type="email" name="email" placeholder="email">
-                        </div>
-
-                        <textarea name="comment" placeholder="comment"></textarea>
-                        <input type="submit" value="send">
-                    </form>
                 </div>
-            </div>
+                <div class="action_buttons">
+                    <div id="{{"displayComments" . $product->id}}" onclick="displayComments({{$product->id}})" class="comments_toggle">
+                        Show Comments
+                    </div>
+                    <div id="{{$product->id . "commentContainer"}}" class="comments">
+                        @foreach($product->approvedComments as $comment)
+                            <div class="comment">
+                                <div class="comment_signature">
+                                    <p>{{$comment->name}} {{$comment->email}}</p>
+                                    <p>{{\Carbon\Carbon::parse($comment->created_at)->format("Y-m-d")}}</p>
+                                </div>
+                                <p>{{$comment->comment}}</p>
+                            </div>
+                        @endforeach
+                        <form method="POST" action="{{route("add.comment")}}" class="comment">
+                            {{csrf_field()}}
+                            <input type="hidden" name="product_id" value="{{$product->id}}">
+                            <div class="comment_signature">
+                                <input type="text" name="name" placeholder="name">
+                                <input type="email" name="email" placeholder="email">
+                            </div>
 
-        </div>
-    @endforeach
+                            <textarea name="comment" placeholder="comment"></textarea>
+                            <input type="submit" value="send">
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+        @endforeach
+    </div>
+
 <script>
     function displayComments(id){
         if($("#"+id+"commentContainer").css("display")==="flex"){
