c888ecbbfec2c3a6879bf5f113bdd9106bb67348
DroidStudioAS <as.droiddevteam@gmail.com>
2024-04-16 08:17:23 +0200

    reorganized web.php

--- routes/web.php
+++ routes/web.php
@@ -12,17 +12,19 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//userRoutes
 Route::get("/", [\App\Http\Controllers\HomeController::class,"index"])->name("home");
 Route::post("/add/comment",[\App\Http\Controllers\CommentController::class,"addComment"])->name("add.comment");
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']);
 //admin routes
 Route::middleware(["auth", \App\Http\Middleware\admin\AdminMiddleware::class])
     ->prefix("/admin")
+    ->name("admin")
     ->group(function (){
-        Route::get("", [\App\Http\Controllers\admin\CommentController::class, "index"])->name("admin");
-        Route::post("/comment/{comment}",[\App\Http\Controllers\admin\CommentController::class, "toggleCommentStatus"])->name("admin.comment");
+        Route::get("", [\App\Http\Controllers\admin\CommentController::class, "index"]);
+        Route::post("/comment/{comment}",[\App\Http\Controllers\admin\CommentController::class, "toggleCommentStatus"])->name(".comment");
     });
+
+Auth::routes();
+
+
