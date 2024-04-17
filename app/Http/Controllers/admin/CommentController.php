e6215ca9c026ceff07abd91e546715ed94d922ce
DroidStudioAS <as.droiddevteam@gmail.com>
2024-04-17 07:00:55 +0200

    created comment repository and placed the comment filtering logic in it

--- app/Http/Controllers/admin/CommentController.php
+++ app/Http/Controllers/admin/CommentController.php
@@ -4,10 +4,17 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\CommentModel;
+use App\Repositories\CommentRepository;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
+    private $commentRepo;
+    public function __construct()
+    {
+        $this->commentRepo=new CommentRepository();
+    }
+
     public function index(){
         $comments = CommentModel::all();
 
@@ -27,11 +34,7 @@ class CommentController extends Controller
     public function filterComments(Request $request){
         $status = $request->status===null ? "" : $request->status;
 
-        $comments = CommentModel::where("name","LIKE","%$request->name%")
-            ->where("email","LIKE","%$request->email%")
-            ->where("comment","LIKE","%$request->comment%")
-            ->where("status", "LIKE", "%$request->status%")
-            ->get();
+        $comments = $this->commentRepo->searchComments($request,$status);
 
 
         return view("admin.admin_dash", compact("comments"));
---
+++ app/Repositories/CommentRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\CommentModel;
+use Illuminate\Http\Request;
+
+class CommentRepository{
+    private $commentModel;
+    public function __construct()
+    {
+        $this->commentModel=new CommentModel();
+    }
+    public function searchComments(Request $request, $status)
+    {
+        $this->commentModel=CommentModel::where("name","LIKE","%$request->name%")
+            ->where("email","LIKE","%$request->email%")
+            ->where("comment","LIKE","%$request->comment%")
+            ->where("status", "LIKE", "%$status%")
+            ->get();
+
+        return $this->commentModel;
+    }
+
+}
