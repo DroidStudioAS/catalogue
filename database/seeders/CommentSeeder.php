e86fa4b8064eee17a743a6b4f546ce218ce57a49
DroidStudioAS <as.droiddevteam@gmail.com>
2024-04-17 14:40:56 +0200

    comment seeder now prompts you to enter wether you want to enter only approved, random or pending comments

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -16,16 +16,30 @@ class CommentSeeder extends Seeder
      */
     public function run()
     {
+        $status="";
         $numOfComments = $this->command->getOutput()->ask("How many comments to you want to generate per post", 3);
+        $choiceOfCommentStatus = intval($this->command->getOutput()->ask("Do you want to randomize the status of generated comments, or generate only approved or banned comments? [1,2,3]",0));
+        if($choiceOfCommentStatus<1 || $choiceOfCommentStatus>3){
+            $this->command->getOutput()->error("You must choose 0, 1 or 2");
+            die();
+        }
+        if($choiceOfCommentStatus===2){
+            $status="approved";
+        }else if($choiceOfCommentStatus===3){
+            $status="pending";
+        }
         $faker = Factory::create();
         foreach (ProductModel::all() as $product){
             for($i=0; $i<$numOfComments; $i++){
+                if($choiceOfCommentStatus===1){
+                    $status=rand(0,1)===1?"approved":"pending";
+                }
                 CommentModel::create([
                     "product_id"=>$product->id,
                     "name"=>$faker->name(),
                     "email"=>$faker->email(),
                     "comment"=>$faker->paragraph(1),
-                    "status"=>"approved"
+                    "status"=>$status
                 ]);
             }
         }
