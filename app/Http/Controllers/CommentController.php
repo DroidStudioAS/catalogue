f4c866c6530e97b484104f84340a48651253e55b
DroidStudioAS <as.droiddevteam@gmail.com>
2024-04-17 07:13:36 +0200

    added addCommentRequest and implemented it

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,19 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AddCommentRequest;
 use App\Models\CommentModel;
 use App\Models\ProductModel;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    public function addComment( Request $request){
-        $request->validate([
-            "product_id"=>"required|int|gte:1",
-            "name"=>"required|string",
-            "email"=>"required|email",
-            "comment"=>"required|string"
-        ]);
+    public function addComment(AddCommentRequest $request){
+
         $comment = CommentModel::create($request->except("_token"));
 
         return redirect()->back()->with("message", "Comment Posted Successfully... Pending Approval");
